53082b1afc3f64e8b5e3cf9d09462ac93bfffd98
IvanFedotov333 <ivan_fedotov_2013@inbox.ru>
2026-08-13 12:26:49 +0300

    дз №19

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,36 +1,38 @@
 const timer = (deadline) => {
-  console.log(deadline);
-
   const hours = document.getElementById("timer-hours");
   const minutes = document.getElementById("timer-minutes");
   const seconds = document.getElementById("timer-seconds");
+  const addZero = (num) => String(num).padStart(2, "0");
 
   const getTimeRemaining = () => {
     let stopDate = new Date(deadline).getTime();
     let currentDate = new Date().getTime();
     let timeRemaining = (stopDate - currentDate) / 1000;
-    let screenHours = Math.floor(timeRemaining / 60 / 60);
-    let screenMinutes = Math.floor((timeRemaining / 60) % 60);
-    let screenSeconds = Math.floor(timeRemaining % 60);
 
     return {
       timeRemaining,
-      screenHours,
-      screenMinutes,
-      screenSeconds,
+      screenHours: Math.floor(timeRemaining / 3600),
+      screenMinutes: Math.floor((timeRemaining / 60) % 60),
+      screenSeconds: Math.floor(timeRemaining % 60),
     };
   };
 
   const updateClock = () => {
     let getTime = getTimeRemaining();
 
-    hours.textContent = getTime.screenHours;
-    minutes.textContent = getTime.screenMinutes;
-    seconds.textContent = getTime.screenSeconds;
-    if (getTime.timeRemaining > 0) {
-      setTimeout(updateClock, 1000);
+    hours.textContent = addZero(getTime.screenHours);
+    minutes.textContent = addZero(getTime.screenMinutes);
+    seconds.textContent = addZero(getTime.screenSeconds);
+
+    if (getTime.timeRemaining <= 0) {
+      clearInterval(intevralId);
+      hours.textContent = "00";
+      minutes.textContent = "00";
+      seconds.textContent = "00";
+      return;
     }
   };
+  const intervalId = setInterval(updateClock, 1000);
   updateClock();
 };
 
