3566b2eaa077155ab1074ee94cac3bc20d1c6977
IvanFedotov333 <ivan_fedotov_2013@inbox.ru>
2026-08-17 22:55:25 +0300

    homework21

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import smoothScroll from "./modules/scroll";
+import inputValidation from "./modules/validation";
 
 timer("21 december 2026");
 menu();
 modal();
+inputValidation();
 
 const scrollButton = document.querySelector('a[href="#service-block"]');
 scrollButton.addEventListener("click", (e) => {
