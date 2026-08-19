b668d652bbf2e203fb7937e4b127828910a47fa4
IvanFedotov333 <ivan_fedotov_2013@inbox.ru>
2026-08-19 14:23:42 +0300

    homework22_hard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import timer from "./modules/timer";
-import menu from "./modules/menu";
+import toggleMenu from "./modules/menu";
 import modal from "./modules/modal";
 import smoothScroll from "./modules/scroll";
 import inputValidation from "./modules/validation";
 import tabs from "./modules/tabs";
 
 timer("21 december 2026");
-menu();
+toggleMenu();
 modal();
 inputValidation();
 tabs();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,28 +1,32 @@
 import smoothScroll from "./scroll";
 
-const menu = () => {
-  const btnMenu = document.querySelector(".menu");
-  const listMenu = document.querySelector("menu");
+const toggleMenu = () => {
+  document.addEventListener("click", (e) => {
+    const btnMenu = document.querySelector(".menu");
+    const listMenu = document.querySelector("menu");
 
-  btnMenu.addEventListener("click", () => {
-    listMenu.classList.toggle("active-menu");
-  });
-
-  listMenu.addEventListener("click", (e) => {
-    if (e.target.closest(".close-btn")) {
-      listMenu.classList.remove("active-menu");
+    if (e.target.closest(".menu")) {
+      listMenu.classList.toggle("active-menu");
       return;
     }
-    const menuLink = e.target.closest("ul>li>a");
-    if (menuLink) {
-      e.preventDefault();
-      const targetId = menuLink.getAttribute("href").slice(1);
-      const target = document.getElementById(targetId);
-      if (target) {
-        smoothScroll(target);
+    if (e.target.closest("menu")) {
+      if (e.target.closest(".close-btn")) {
         listMenu.classList.remove("active-menu");
+        return;
       }
+      const menuLink = e.target.closest("ul>li>a");
+      if (menuLink) {
+        e.preventDefault();
+        const targetId = menuLink.getAttribute("href").slice(1);
+        const target = document.getElementById(targetId);
+        if (target) {
+          smoothScroll(target);
+          listMenu.classList.remove("active-menu");
+        }
+      }
+      return;
     }
+    listMenu.classList.remove("active-menu");
   });
 };
-export default menu;
+export default toggleMenu;
